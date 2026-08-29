6a8cdf2a8c17355dababb6d3081cc72612982680
nlogic-07 <choprajaskaran76@gmail.com>
2026-08-29 19:41:02 +0530

    userSchema

---
+++ server/models/user.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      maxLength: [50, "Name cannot exceed 50 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+      minlength: [8, "Password must be atleast 8 characters long"],
+    },
+    role: {
+      type: String,
+      default: "Student",
+      enum: ["Student", "Teacher", "Admin"],
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+
+    department: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+    experties: {
+      type: [String],
+      default: [],
+    },
+    maxStudents: {
+      type: Number,
+      default: 10,
+      min: [1, "Minimum students must be at least 1"],
+    },
+    assignedStudents: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    supervisor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
